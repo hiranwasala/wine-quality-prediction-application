31ba54f21b9e2a9b92f91a9cb742c942aea76d55
W.A.H.U.W Arachchi <145230618+hiranwasala@users.noreply.github.com>
2024-12-17 09:49:44 +0530

    Update HomePage.jsx

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -40,7 +40,7 @@ const HomePage = () => {
         
         try {
           // Send the features to the Flask backend via a POST request
-          const response = await fetch('http://127.0.0.1:5000/predict', {
+          const response = await fetch('https://wine-quality-prediction-y1hr.onrender.com/predict', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
